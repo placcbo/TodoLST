903650a8645391b4b0b682aa836d0f30d963ce79
kevin ndirangu <placbo2@gmail.com>
2026-05-05 11:21:57 +0300

    feat:add todo

--- todo.go
+++ todo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"time"
 )
@@ -27,4 +28,15 @@ func (i CreateTodoInput) Validate() error {
 		return ErrEmptyTitle
 	}
 	return nil
+}
+
+type UpdateTodoInput struct {
+	Done bool `json:"done"`
+}
+type TodoStore interface {
+	Create(ctx context.Context, input CreateTodoInput) (Todo, error)
+	GetAll(ctx context.Context) ([]Todo, error)
+	GetByID(ctx context.Context, id int64) (Todo, error)
+	Update(ctx context.Context, id int64, input UpdateTodoInput) (Todo, error)
+	Delete(ctx context.Context, id int64) error
 }
