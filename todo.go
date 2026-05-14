dbcdc7ac1685e43532efbea935f9330947a84182
kevin ndirangu <placbo2@gmail.com>
2026-05-14 11:34:18 +0300

    ...

--- todo.go
+++ todo.go
@@ -1,82 +1,41 @@
 package main
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"time"
-)
-
-var (
-	ErrEmptyTitle = errors.New("title cannot be empty")
-)
+import "fmt"
 
 type Todo struct {
-	Title       string    `json:"title"`
-	ID          int64     `json:"id"`
-	Done        bool      `json:"done"`
-	CompletedAt time.Time `json:"completed_at"`
+	Title string
+	ID    int64
+	Done  bool
 }
 
-type CreateTodoInput struct {
-	Title string `json:"title"`
+type MemoryStore struct {
+	todos  []Todo
+	NextID int64
 }
 
-func (t CreateTodoInput) Validate() error {
-	if t.Title == "" {
-		return ErrEmptyTitle
+func (m *MemoryStore) Create(title string) {
+	m.NextID++
+	todo := Todo{
+		ID:    m.NextID,
+		Title: title,
+		Done:  false,
 	}
-	return nil
-}
-
-type UpdateTodoInput struct {
-	Done bool `json:"done"`
-}
-
-type TodoStore interface {
-	Create(ctx context.Context, input CreateTodoInput) (Todo, error)
-	GetAll(ctx context.Context) ([]Todo, error)
-	GetByID(ctx context.Context, id int64) (Todo, error)
-	Update(ctx context.Context, id int64, input UpdateTodoInput) (Todo, error)
-	Delete(ctx context.Context, id int64) error
+	m.todos = append(m.todos, todo)
 }
 
 func main() {
-	fmt.Println("====CHECKPOINT1: Create a todo manually")
-	t1 := Todo{ID: 1, Title: "learn go", Done: false}
-	fmt.Println("Todo:", t1)
-	fmt.Println("ID:", t1.ID)
-	fmt.Println("Done:", t1.Done)
+	store := MemoryStore{}
+	store.Create("learn golang")
+	store.Create("learn API")
+	store.Create("learn Todo")
+	store.Create("learn postgres")
+	store.Create("learn docker")
 
-	fmt.Println("===CHECKPOINT2=== validate a valid input")
-	input1 := CreateTodoInput{
-		Title: "learn postgress",
+
+	for _, todo := range store.todos{
+		fmt.Println(todo.ID, todo.Title)
 	}
-	err := input1.Validate()
-	fmt.Println("Input:", input1)
-	fmt.Println("validation error: ", err)
 
-	fmt.Println("===CHECKPOINT3=== validate a valid input")
-	input2 := CreateTodoInput{
-		Title: "",
-	}
-	err1 := input2.Validate()
-	fmt.Println("input:", input2)
-	fmt.Println("validation error:", err1)
+	
 
-	fmt.Println("\n===== CHECKPOINT 4: UpdateTodoInput =====")
-
-	update1 := UpdateTodoInput{
-		Done: true,
-	}
-	fmt.Println("Update input:", update1)
-	fmt.Println("New done value:", update1.Done)
-
-	fmt.Println("\n===== CHECKPOINT 5: Simulate updating t1 =====")
-	t1.Done = update1.Done
-	t1.CompletedAt = time.Now()
-
-	fmt.Println("Updated Todo:", t1)
-	fmt.Println("Done:", t1.Done)
-	fmt.Println("CompletedAt:", t1.CompletedAt)
 }
